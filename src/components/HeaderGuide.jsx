3b4bb49a4612ae0dda209f4caba60f152df2653e
KazutoKitaoka7199 <wrkitaokakazuto@gmail.com>
2022-03-24 00:18:48 +0900

    HeaderGuideのスタイル調整

--- src/components/HeaderGuide.jsx
+++ src/components/HeaderGuide.jsx
@@ -4,34 +4,60 @@ import {
 } from 'react-native';
 
 const Line = require('../../img/Line.png');
+const Ellipse = require('../../img/Ellipse.png');
 
 export default function HeaderGuide() {
-  const assetImg = Image.resolveAssetSource(Line);
+  const lineImg = Image.resolveAssetSource(Line);
+  const ellipseImg = Image.resolveAssetSource(Ellipse);
   return (
     <View>
-      <View>
+      <Image
+        source={Line}
+        style={[styles.line, {
+          transform: [
+            { translateX: -lineImg.width / 2 },
+            { translateY: -lineImg.height / 2 },
+          ],
+        }]}
+      />
+      <View style={styles.dot}>
         <Image
-          source={Line}
-          style={[styles.line, {
+          source={Ellipse}
+          style={[styles.ellipse, {
             transform: [
-              { translateX: -assetImg.width / 2 },
-              { translateY: -assetImg.height / 2 },
+              { translateX: -ellipseImg.width / 2 },
+              { translateY: -ellipseImg.height / 2 },
             ],
           }]}
         />
-        <View style={styles.dot}>
-          <Text>・</Text>
-          <Text>・</Text>
-          <Text>・</Text>
-        </View>
-        <Text>step1</Text>
-        <Text>step2</Text>
-        <Text>step3</Text>
+        <Image
+          source={Ellipse}
+          style={[styles.ellipse, {
+            transform: [
+              { translateX: -ellipseImg.width / 2 },
+              { translateY: -ellipseImg.height / 2 },
+            ],
+          }]}
+        />
+        <Image
+          source={Ellipse}
+          style={[styles.ellipse, {
+            transform: [
+              { translateX: -ellipseImg.width / 2 },
+              { translateY: -ellipseImg.height / 2 },
+            ],
+          }]}
+        />
+      </View>
+      <View style={styles.steps}>
+        <Text style={styles.step1}>step1</Text>
+        <Text style={styles.step2}>step2</Text>
+        <Text style={styles.step3}>step3</Text>
       </View>
       <View>
-        <Text>基本設定</Text>
-        <Text>ポートフォリオ作成</Text>
-        <Text>入金設定</Text>
+        <Text style={styles.guide1}>基本設定</Text>
+        <Text style={styles.guide2}>ポートフォリオ作成</Text>
+        <Text style={styles.guide3}>入金設定</Text>
       </View>
     </View>
   );
@@ -43,8 +69,36 @@ const styles = StyleSheet.create({
     left: '50%',
   },
   dot: {
-    width: '100%',
     flexDirection: 'row',
     justifyContent: 'space-around',
+  },
+  steps: {
+    position: 'absolute',
+  },
+  step1: {
+    left: 45,
+  },
+  step2: {
+    position: 'absolute',
+    left: 177.5,
+  },
+  step3: {
+    position: 'absolute',
+    left: 305,
+  },
+  guide1: {
+    left: 30,
+    position: 'absolute',
+    lineHeight: 30,
+  },
+  guide2: {
+    left: 130,
+    position: 'absolute',
+    lineHeight: 30,
+  },
+  guide3: {
+    left: 300,
+    position: 'absolute',
+    lineHeight: 30,
   },
 });
