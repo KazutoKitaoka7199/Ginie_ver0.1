37ec3024f8cc849e6aca1d52a541a7530d1a53eb
KazutoKitaoka7199 <wrkitaokakazuto@gmail.com>
2022-03-25 02:17:49 +0900

    markup and style InvestmentSettingExpert01

--- App.jsx
+++ App.jsx
@@ -3,12 +3,14 @@ import { View } from 'react-native';
 // import SignIn from './src/screens/SignIn';
 // import Landing from './src/screens/Landing';
 // import StockApi from './src/screens/StockApi';
-import AddProduct from './src/screens/AddProduct';
+// import AddProduct from './src/screens/AddProduct';
+// import BalanceProduct from './src/screens/BalanceProduct';
+import InvestmentSettingExpert01 from './src/screens/InvestmentSettingExpert_01';
 
 export default function App() {
   return (
     <View>
-      <AddProduct />
+      <InvestmentSettingExpert01 />
     </View>
   );
 }
